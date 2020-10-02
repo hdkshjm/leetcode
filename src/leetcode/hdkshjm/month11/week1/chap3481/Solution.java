ea2eebe0c481338aabc69ca59ec8c91527dc5cac
Hideaki Ishijima <hideaki.ishijima@linecorp.com>
2020-10-03 00:14:59 +0900

    fix: remove cache
    Runtime: 11 ms
    Memory Usage: 40 MB

--- src/leetcode/hdkshjm/month11/week1/chap3481/Solution.java
+++ src/leetcode/hdkshjm/month11/week1/chap3481/Solution.java
@@ -3,91 +3,54 @@ package leetcode.hdkshjm.month11.week1.chap3481;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Queue;
 
 class Solution {
-    HashMap<Integer, List<Integer>> cache = new HashMap<Integer, List<Integer>>();
-    Queue<Integer> unchekedQueue = new ArrayDeque<Integer>();
 
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         Arrays.sort(candidates);
-        unchekedQueue.add(target);
-        while (true) {
-            Integer i = unchekedQueue.poll();
-            if (i == null)
+
+        ArrayDeque<ArrayDeque<Integer>> queue = new ArrayDeque<ArrayDeque<Integer>>();
+        for (int c : candidates) {
+            ArrayDeque<Integer> newCombination = new ArrayDeque<Integer>();
+            int remain = target - c;
+            if (remain < 0) {
                 break;
-            createCache(candidates, i);
+            }
+            
+            newCombination.addFirst(target - c);// index=0には残りをいれる
+            newCombination.addLast(c);
+            queue.add(newCombination);
         }
 
-
-        ArrayDeque<List<Integer>> queue = new ArrayDeque<List<Integer>>();
-        for (int c : cache.get(target)) {
-            List<Integer> newList = new ArrayList<Integer>();
-            newList.add(target - c);// index=0には残りをいれる
-            newList.add(c);
-            queue.add(newList);
-        }
-
-        
         List<List<Integer>> ret = new ArrayList<List<Integer>>();
-        while (true) {
-            List<Integer> list = queue.poll();
-            if (list == null) {
-                break;
-            }
+        while (!queue.isEmpty()) {
+            ArrayDeque<Integer> combination = queue.poll();
 
-            int remain = list.get(0);
+            int remain = combination.getFirst();
             if (remain == 0) {
-                list.remove(0);
-                ret.add(list);
+                combination.removeFirst();
+                ret.add(new ArrayList<Integer>(combination));
                 continue;
             }
 
-            List<Integer> nextCandidates = cache.get(remain);
-            if (nextCandidates == null || nextCandidates.size() == 0) {
-                continue;
-            }
-            for (int c : nextCandidates) {
-                if (c < list.get(list.size() - 1)) {
+            for (int c : candidates) {
+                if (c < combination.getLast()) {
                     continue;
                 }
+                if (remain - c < 0) {
+                    break;
+                }
                 
-                List<Integer> newList = new ArrayList<Integer>(list);
-                newList.remove(0);
-                newList.add(0, remain - c);
-                newList.add(c);
-                queue.add(newList);
+                ArrayDeque<Integer> newCombination = new ArrayDeque<Integer>(combination);
+                newCombination.removeFirst();
+                newCombination.addFirst(remain - c);
+                newCombination.addLast(c);
+
+                queue.addFirst(newCombination);
             }
         }
         return ret;
     }
 
-    public boolean createCache(int[] candidates, int target) {
-        List<Integer> list = new ArrayList<Integer>();
-
-        for (int c : candidates) {
-            int newTarget = target - c;
-
-            if (newTarget < 0) {
-                break;
-            }
-            
-            if (newTarget == 0) {
-                list.add(c);
-                break;
-            }
-
-            if (newTarget > 0) {
-                if (!cache.containsKey(newTarget)) {
-                    unchekedQueue.add(newTarget);
-                }
-                list.add(c);
-            }
-        }
-        cache.put(target, list);
-
-        return list.size() > 0;
-    }
 }
