15c1284100fd7b7088b3e6474b4629e9c9f7cdfc
Hideaki Ishijima <hideaki.ishijima@linecorp.com>
2020-10-03 02:11:11 +0900

    add: add problems/roman-to-integer
    Runtime: 5 ms, faster than 67.62% of Java online submissions for Roman to Integer.
    Memory Usage: 39.1 MB, less than 97.69% of Java online submissions for Roman to Integer.

---
+++ src/leetcode/hdkshjm/problems/roman_to_integer/Solution.java
@@ -0,0 +1,77 @@
+package leetcode.hdkshjm.problems.roman_to_integer;
+
+class Solution {
+    public int romanToInt(String s) {
+        int ret = 0;
+
+        int[] thousandsPlace = convert(s, "M", 1000, "A", 0, "B", 0);
+        s = s.substring(thousandsPlace[0]);
+        ret = ret + thousandsPlace[1];
+
+        int[] hundredsPlace = convert(s, "C", 100, "D", 500, "M", 1000);
+        s = s.substring(hundredsPlace[0]);
+        ret = ret + hundredsPlace[1];
+
+        int[] tensPlace = convert(s, "X", 10, "L", 50, "C", 100);
+        s = s.substring(tensPlace[0]);
+        ret = ret + tensPlace[1];
+
+        int[] onesPlace = convert(s, "I", 1, "V", 5, "X", 10);
+        s = s.substring(onesPlace[0]);
+        ret = ret + onesPlace[1];
+        
+        return ret;
+    }
+
+    public int[] convert(String s,
+            String multiple1String, int multiple1Value,
+            String multiple5String, int multiple5Value, 
+            String multiple10String, int multiple10Value) {
+
+        char multiple1 = multiple1String.charAt(0);
+        char multiple5 = multiple5String.charAt(0);
+        char multiple10 = multiple10String.charAt(0);
+
+        if (s.length() > 1) {
+            if (s.charAt(0) == multiple1 && s.charAt(1) == multiple10) { // 9
+                return new int[] { 2, multiple10Value - multiple1Value };
+            }
+            if (s.charAt(0) == multiple1 && s.charAt(1) == multiple5) { // 4
+                return new int[] { 2, multiple5Value - multiple1Value };
+            }
+        }
+
+        int index = 0;
+        int ret = 0;
+        while (true) {
+            if (index >= s.length()) {
+                break;
+            }
+
+            char c = s.charAt(index);
+
+            if (c == multiple1) {
+                ret = ret + multiple1Value;
+                index++;
+                continue;
+            }
+
+            if (c == multiple5) {
+                ret = ret + multiple5Value;
+                index++;
+                continue;
+            }
+
+            if (c == multiple10) {
+                ret = ret + multiple10Value;
+                index++;
+                continue;
+            }
+            
+            break;
+        }
+
+        return new int[] { index, ret };
+        
+    }
+}
---
+++ src/leetcode/hdkshjm/problems/roman_to_integer/TestSolution.java
@@ -0,0 +1,27 @@
+package leetcode.hdkshjm.problems.roman_to_integer;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class TestSolution {
+
+    Solution solution = new Solution();
+
+    static Stream<Arguments> testProvider() {
+        return Stream.of(
+                Arguments.of("III", 3),
+                Arguments.of("MCMXCIV", 1994)
+                );
+    }
+
+    @ParameterizedTest
+    @MethodSource("testProvider")
+    void test(String s, int expected) {
+        int result = solution.romanToInt(s);
+        Assertions.assertEquals(expected, result);
+    }
+}
