14e92cb66fc37f035831cf63d341aacb0b72685e
Hideaki Ishijima <hideaki.ishijima@linecorp.com>
2020-10-04 02:22:09 +0900

    fix: use HashMap
    Runtime: 9 ms
    Memory Usage: 39 MB

--- src/leetcode/hdkshjm/month11/week1/chap3482/Solution.java
+++ src/leetcode/hdkshjm/month11/week1/chap3482/Solution.java
@@ -1,27 +1,31 @@
 package leetcode.hdkshjm.month11.week1.chap3482;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 class Solution {
     public int findPairs(int[] nums, int k) {
         Arrays.sort(nums);
-
-        int count = 0;
-        int previous = nums[0] - 1;
-        int index = 1;
-        for (int i = 0; i < nums.length - 1; i++) {
-            if (previous == nums[i]) {
-                continue;
-            }
-            for (int j = Math.max(index, i + 1); j < nums.length; j++) {
-                if ((nums[i] + k) == nums[j]) {
-                    count++;
-                    index = j + 1;
-                    break;
-                }
-            }
-            previous = nums[i];
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
         }
+        
+        int count = 0;
+
+        if (k == 0) {
+            for (int n: map.keySet()) {
+                if (map.get(n) > 1)
+                    count++;
+            }
+        } else {
+            for (int n : map.keySet()) {
+                if (map.containsKey((n + k)))
+                    count++;
+            }
+        }
+
         return count;
     }
 }
