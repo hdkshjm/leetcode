07174194bd71a5c7ec5f8331ce3db218ddd94a37
Hideaki Ishijima <hideaki.ishijima@linecorp.com>
2020-10-04 23:06:49 +0900

    add: add october week1 3483
    Runtime: 9 ms
    Memory Usage: 39.3 MB

---
+++ src/leetcode/hdkshjm/month11/week1/chap3483/Solution.java
@@ -0,0 +1,31 @@
+package leetcode.hdkshjm.month11.week1.chap3483;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.TreeMap;
+
+class Solution {
+    public int removeCoveredIntervals(int[][] intervals) {
+
+        Map<Integer,Integer> map = new TreeMap<Integer,Integer>();
+        for (int i = 0; i < intervals.length; i++) {
+            int end = map.getOrDefault(intervals[i][0], intervals[i][1]) ;
+            map.put(intervals[i][0], Math.max(intervals[i][1], end));
+        }
+
+        Iterator<Map.Entry<Integer, Integer>> iterator = map.entrySet().iterator();
+        Map.Entry<Integer, Integer> entry = iterator.next();
+        int[] previous = new int[] { entry.getKey(), entry.getValue() };
+
+        int count = 1;
+        while (iterator.hasNext()) {
+            entry = iterator.next();
+            if (previous[0] > entry.getKey() || previous[1] < entry.getValue()) {
+                previous[0] = entry.getKey();
+                previous[1] = entry.getValue();
+                count++;
+            }
+        }
+        return count;
+    }
+}
