e2568599437a743af7f2391ceb3df729c0647402
Hideaki Ishijima <hideaki.ishijima@linecorp.com>
2020-10-05 14:16:55 +0900

    fix: rename package name

---
+++ src/leetcode/hdkshjm/month10/week3_3463/TestSolution.java
@@ -0,0 +1,30 @@
+package leetcode.hdkshjm.month10.week3_3463;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class TestSolution {
+
+    Solution solution = new Solution();
+
+    static Stream<Arguments> testProvider() {
+        return Stream.of(
+                Arguments.of("GGLLGG", true),
+                Arguments.of("GG", false),
+                Arguments.of("GL", true),
+                Arguments.of("LLGRL", true)
+                );
+    }
+
+    @ParameterizedTest
+    @MethodSource("testProvider")
+    void isRobotBounded(String instructions, boolean expected) {
+        boolean result = solution.isRobotBounded(instructions);
+        Assertions.assertEquals(expected, result);
+
+    }
+}
