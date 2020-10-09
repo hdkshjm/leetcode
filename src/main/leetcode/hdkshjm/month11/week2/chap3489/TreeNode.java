707ca06c355a717e9c79691d8aa2455d9aaf1588
Hideaki Ishijima <hideaki.ishijima@linecorp.com>
2020-10-09 21:52:15 +0900

    add: add october week2 chap 3489 Runtime: 8 ms Memory Usage: 39.6 MB

---
+++ src/main/leetcode/hdkshjm/month11/week2/chap3489/TreeNode.java
@@ -0,0 +1,11 @@
+package leetcode.hdkshjm.month11.week2.chap3489;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
