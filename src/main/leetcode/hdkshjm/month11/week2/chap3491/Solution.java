b36c18c5ae2ef226d426794e308c1dbe5cb8c45a
Hideaki Ishijima <hideaki.ishijima@linecorp.com>
2020-10-12 14:14:12 +0900

    fix: do not use brute force Runtime: 5 ms Memory Usage: 39.1 MB

--- src/main/leetcode/hdkshjm/month11/week2/chap3491/Solution.java
+++ src/main/leetcode/hdkshjm/month11/week2/chap3491/Solution.java
@@ -1,76 +1,44 @@
 package leetcode.hdkshjm.month11.week2.chap3491;
 
-
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.TreeSet;
+import java.util.Stack;
 
 class Solution {
-    String returnWord = null;
-    Map<Integer, Map<Character, Integer>> cache = new HashMap<Integer, Map<Character, Integer>>();
+    Map<Character, Integer> countMap = new HashMap<Character, Integer>();
 
     public String removeDuplicateLetters(String s) {
-        TreeSet<Character> set = new TreeSet<Character>();
         for (char c : s.toCharArray()) {
-            set.add(c);
+            countMap.put(c, countMap.getOrDefault(c, 0) + 1);
         }
 
-        String unDuplicateS = new String();
-        for (char c : set) {
-            unDuplicateS = unDuplicateS + c;
-        }
-        String word = new String();
+        Stack<Character> stack = new Stack<Character>();
 
-        find(s, unDuplicateS, word);
-
-        return returnWord;
-    }
-
-    public boolean isIncluded(String s, int fromIndex, String word) {
-        Map<Character, Integer> map = cache.get(fromIndex);
-        if (map == null) {
-            map = new HashMap<Character, Integer>();
-            cache.put(fromIndex, map);
-        }
-        int index;
-        if (map.containsKey(word.charAt(0))) {
-            index = map.get(word.charAt(0));
-        } else {
-            index = s.indexOf(word.charAt(0), fromIndex);
-            map.put(word.charAt(0), index);
-        }
-
-        if (index == -1 || index >= s.length()) {
-            return false;
-        } else {
-            if (word.length() > 1) {
-                return isIncluded(s, index + 1, word.substring(1));
-            } else {
-                return true;
+        for (char c : s.toCharArray()) {
+            if (stack.isEmpty()) {
+                stack.push(c);
+                countMap.put(c, countMap.get(c) - 1);
+                continue;
             }
-        }
-    }
 
-    public void find(String s, String unDuplicateS, String word) {
-        if (returnWord != null) {
-            return;
-        }
-
-        if (unDuplicateS.length() <= 1) {
-            String w = word + unDuplicateS;
-
-            boolean result = isIncluded(s, 0, w);
-            if (result)
-                returnWord = w;
-            return;
-        } else {
-            for (int i = 0; i < unDuplicateS.length(); i++) {
-                find(
-                        s,
-                        unDuplicateS.substring(0, i) + unDuplicateS.substring(i + 1, unDuplicateS.length()),
-                        word + unDuplicateS.charAt(i)
-                );
+            if (!stack.contains(c)) {
+                while (!stack.isEmpty() && c < stack.peek()) {
+                    if (countMap.get(stack.peek()) > 0) {
+                        stack.pop();
+                    } else {
+                        break;
+                    }
+                }
+                stack.push(c);
             }
+            countMap.put(c, countMap.get(c) - 1);
         }
+
+        StringBuffer ret = new StringBuffer();
+        for (Character c : new ArrayList<>(stack)) {
+            ret.append(c);
+        }
+        return ret.toString();
     }
 }
