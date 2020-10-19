545fb812b472eafad15094b8beea372b56513076
Hideaki Ishijima <hideaki.ishijima@linecorp.com>
2020-10-20 01:50:41 +0900

    add: add october week3 3500 Runtime: 5 ms Memory Usage: 46.5 MB

---
+++ src/main/leetcode/hdkshjm/month10/week3/chap3500/Solution.java
@@ -0,0 +1,33 @@
+package leetcode.hdkshjm.month10.week3.chap3500;
+
+class Solution {
+    public int minDominoRotations(int[] A, int[] B) {
+        if (A.length == 0 || B.length == 0) {
+            return -1;
+        }
+
+        int countOfA[] = new int[6];
+        int countOfB[] = new int[6];
+        int targetOfA = A[0];
+        int targetOfB = B[0];
+        for (int i = 0; i < A.length; i++) {
+            if (targetOfA != A[i] && targetOfA != B[i]) {
+                targetOfA = -1;
+            }
+            if (targetOfB != A[i] && targetOfB != B[i]) {
+                targetOfB = -1;
+            }
+            if (targetOfA == -1 && targetOfB == -1) {
+                return -1;
+            }
+            countOfA[A[i] - 1]++;
+            countOfB[B[i] - 1]++;
+        }
+
+        int target = targetOfA - 1;
+        if (targetOfA == -1) {
+            target = targetOfB - 1;
+        }
+        return Math.min(A.length - countOfA[target], B.length - countOfB[target]);
+    }
+}
---
+++ src/test/leetcode/hdkshjm/month10/week3/chap3500/TestSolution.java
@@ -0,0 +1,26 @@
+package leetcode.hdkshjm.month10.week3.chap3500;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class TestSolution {
+    Solution solution = new Solution();
+
+    static Stream<Arguments> testProvider() {
+        return Stream.of(
+                Arguments.of(new int[]{2, 1, 2, 4, 2, 2}, new int[]{5, 2, 6, 2, 3, 2}, 2),
+                Arguments.of(new int[]{3, 5, 1, 2, 3}, new int[]{3, 6, 3, 3, 4}, -1)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("testProvider")
+    void test(int[] A, int[] B, int expected) {
+        int actual = solution.minDominoRotations(A, B);
+        Assertions.assertEquals(expected, actual);
+    }
+}
