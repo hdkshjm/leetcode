5c5faad616d8eaf3af77cadc6aa88ab1a345b22d
Hideaki Ishijima <hideaki.ishijima@linecorp.com>
2020-10-23 23:22:18 +0900

    fix: fix october week4 3505 Runtime: 9 ms Memory Usage: 38.9 MB

--- src/main/leetcode/hdkshjm/challenge/month10/week4/chap3505/Solution.java
+++ src/main/leetcode/hdkshjm/challenge/month10/week4/chap3505/Solution.java
@@ -1,34 +1,25 @@
 package leetcode.hdkshjm.challenge.month10.week4.chap3505;
 
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
 class Solution {
     public boolean find132pattern(int[] nums) {
-        Map<Integer, Integer> countMap = new TreeMap<>();
         int[] minNumsFromStart = new int[nums.length];
         minNumsFromStart[0] = nums[0];
         for (int i = 1; i < minNumsFromStart.length; i++) {
-            countMap.put(nums[i], countMap.getOrDefault(nums[i], 0) + 1);
             minNumsFromStart[i] = Math.min(minNumsFromStart[i - 1], nums[i]);
         }
 
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] > minNumsFromStart[i - 1]) {
-                for (int k : countMap.keySet()) {
-                    if (nums[i] > k && minNumsFromStart[i - 1] < k) {
-                        return true;
-                    }
-                }
+        TreeSet<Integer> set = new TreeSet<>();
+        set.add(nums[nums.length -1]);
+        for (int j = nums.length -2 ; j >= 0 ; j--) {
+            Integer kOfNums = set.lower(nums[j]);
+            set.add(nums[j]);
+            if( kOfNums == null ) continue;
+            if( minNumsFromStart[j] < kOfNums  && kOfNums < nums[j]  ){
+                return true;
             }
-
-            int cnt = countMap.get(nums[i]);
-            if (cnt == 1) {
-                countMap.remove(nums[i]);
-            } else {
-                countMap.put(nums[i], cnt - 1);
-            }
-        }
+         }
         return false;
     }
 }
