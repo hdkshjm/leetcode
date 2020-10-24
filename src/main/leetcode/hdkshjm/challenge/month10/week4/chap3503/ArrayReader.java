a5b43c0b28cb7d26ba704af4e41f9b11724158a3
Hideaki Ishijima <hideaki.ishijima@linecorp.com>
2020-10-25 01:12:02 +0900

    add: add october week4 3505 Runtime: 2 ms Memory Usage: 51.4 MB

---
+++ src/main/leetcode/hdkshjm/challenge/month10/week4/chap3503/ArrayReader.java
@@ -0,0 +1,5 @@
+package leetcode.hdkshjm.challenge.month10.week4.chap3503;
+
+interface ArrayReader {
+    public int get(int index) ;
+}
---
+++ src/test/leetcode/hdkshjm/challenge/month10/week4/chap3503/TestSolution.java
@@ -0,0 +1,28 @@
+package leetcode.hdkshjm.challenge.month10.week4.chap3503;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class TestSolution {
+    Solution solution = new Solution();
+
+    static Stream<Arguments> testProvider() {
+        return Stream.of(
+                Arguments.of(new int[]{-1, 0, 3, 5, 9, 12}, 9, 4),
+                Arguments.of(new int[]{-1, 0, 3, 5, 9, 12}, 2, -1)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("testProvider")
+    void test(int[] nums, int target, int expected) {
+        ArrayReader reader = new ArrayReaderImpl(nums);
+
+        int actual = solution.search(reader, target);
+        Assertions.assertEquals(expected, actual);
+    }
+}
