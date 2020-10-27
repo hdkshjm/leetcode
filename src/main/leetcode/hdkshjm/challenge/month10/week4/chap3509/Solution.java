967ff121605f5278204bfc633060096a635c2a70
Hideaki Ishijima <hideaki.ishijima@linecorp.com>
2020-10-27 23:13:07 +0900

    fix: fix october week4 3509 Runtime: 0 ms Memory Usage: 39.1 MB

--- src/main/leetcode/hdkshjm/challenge/month10/week4/chap3509/Solution.java
+++ src/main/leetcode/hdkshjm/challenge/month10/week4/chap3509/Solution.java
@@ -4,45 +4,37 @@ public class Solution {
     public ListNode detectCycle(ListNode head) {
         if (head == null || head.next == null || head.next.next == null) return null;
 
-        ListNode loopedNode = head;
-        ListNode tempNpde = head.next.next;
-        int lastIndex = 0;
-        for (int i = 1; i <= 100000; i++) {
-            if (loopedNode == tempNpde) {
-                lastIndex = i;
-                break;
-            }
-            loopedNode = loopedNode.next;
-            if (tempNpde.next == null || tempNpde.next.next == null) return null;
-            tempNpde = tempNpde.next.next;
+        ListNode loopedNode1 = head.next;
+        ListNode loopedNode2 = head.next.next;
+        while (loopedNode1 != loopedNode2) {
+            loopedNode1 = loopedNode1.next;
+            if (loopedNode2.next == null || loopedNode2.next.next == null) return null;
+            loopedNode2 = loopedNode2.next.next;
         }
 
-        int loopLength = 1;
-        tempNpde = loopedNode.next;
-        for (int i = 1; i <= 100000; i++) {
-            if (loopedNode == tempNpde) {
-                loopLength = i;
-                break;
-            }
-            tempNpde = tempNpde.next;
-        }
+        /*
+        - 定義
+        loop開始までの距離: S
+        loop内の長さ: L
+        かめ(loopedNode)のloop内の移動距離: X
 
+        - かめ(loopedNode)とうさぎ(tempNode)の交差した点までの距離
+        かめ: S + X mod L
+        うさぎ: S + (S + 2X ) mod L
 
-        int start = Math.max(0, lastIndex - loopLength);
-        for (int i = 0; i < start; i++) {
+        - かめとウサギは出会ってることから
+        X mod L = (S + 2X ) mod L
+        0 = (S + X ) mod L
+
+        なので、カメの交差点までの距離 S + X mod L に Sを足すと
+        S + (X + S)mod L = S
+        head と loopedNodeを一緒に移動させて交わったところがloop開始点になる
+         */
+        while (loopedNode1 != head) {
+            loopedNode1 = loopedNode1.next;
             head = head.next;
         }
 
-        for (int i = start; i <= lastIndex; i++) {
-            ListNode node = loopedNode;
-            for (int j = 0; j <= loopLength; j++) {
-                if (head == node) {
-                    return head;
-                }
-                node = node.next;
-            }
-            head = head.next;
-        }
-        return null;
+        return loopedNode1;
     }
 }
