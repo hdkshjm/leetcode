8c0cfbc0a0695aa2fbe9fec132b1861debf523b2
Hideaki Ishijima <hideaki.ishijima@linecorp.com>
2020-10-29 22:53:41 +0900

    add: add october week5 3512 Runtime: 2 ms Memory Usage: 41.2 MB

---
+++ src/main/leetcode/hdkshjm/challenge/month10/week5/chap3512/Solution.java
@@ -0,0 +1,44 @@
+package leetcode.hdkshjm.challenge.month10.week5.chap3512;
+
+class Solution {
+    public int maxDistToClosest(int[] seats) {
+
+        int[] maxEmptyInterval = new int[]{-1, -1};
+        int[] interval = new int[]{-1, -1};
+        int leftEmptyLength = -1;
+        int rightEmptyLength = -1;
+
+        for (int i = 0; i < seats.length; i++) {
+            if (seats[i] == 0) {
+                if (interval[0] == -1) interval[0] = i;
+                continue;
+            }
+
+            if (interval[0] != -1) {
+                interval[1] = i - 1;
+                if (interval[1] - interval[0] >= maxEmptyInterval[1] - maxEmptyInterval[0]) {
+                    maxEmptyInterval[0] = interval[0];
+                    maxEmptyInterval[1] = interval[1];
+                }
+                if (interval[0] == 0) leftEmptyLength = interval[1] - interval[0] + 1;
+            }
+
+            interval[0] = -1;
+            interval[1] = -1;
+        }
+
+        if (interval[0] != -1) {
+            interval[1] = seats.length - 1;
+            if (interval[1] - interval[0] > maxEmptyInterval[1] - maxEmptyInterval[0]) {
+                maxEmptyInterval[0] = interval[0];
+                maxEmptyInterval[1] = interval[1];
+            }
+            if (interval[1] == seats.length - 1) rightEmptyLength = interval[1] - interval[0] + 1;
+        }
+
+        int max = (maxEmptyInterval[1] - maxEmptyInterval[0]) / 2 + 1;
+        max = Math.max(max, leftEmptyLength);
+        max = Math.max(max, rightEmptyLength);
+        return max;
+    }
+}
---
+++ src/test/leetcode/hdkshjm/challenge/month10/week5/chap3512/TestSolution.java
@@ -0,0 +1,30 @@
+package leetcode.hdkshjm.challenge.month10.week5.chap3512;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class TestSolution {
+    Solution solution = new Solution();
+
+    static Stream<Arguments> testProvider() {
+        return Stream.of(
+                Arguments.of(new int[]{1, 0, 0, 0, 1, 0, 1}, 2),
+                Arguments.of(new int[]{1, 0, 0, 0}, 3),
+                Arguments.of(new int[]{0, 1}, 1),
+                Arguments.of(new int[]{0, 0, 0, 1}, 3)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("testProvider")
+    void test(int[] seats, int expected) {
+        int actual = solution.maxDistToClosest(seats);
+        Assertions.assertEquals(expected, actual);
+    }
+}
