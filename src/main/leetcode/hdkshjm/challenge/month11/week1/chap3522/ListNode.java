a922c318704a8839d7d3e40224e165e37f0a4284
Hideaki Ishijima <hideaki.ishijima@linecorp.com>
2020-11-09 13:47:15 +0900

    add: add october week1 3522 Runtime: 2 ms Memory Usage: 39.7 MB

---
+++ src/main/leetcode/hdkshjm/challenge/month11/week1/chap3522/ListNode.java
@@ -0,0 +1,18 @@
+package leetcode.hdkshjm.challenge.month11.week1.chap3522;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
