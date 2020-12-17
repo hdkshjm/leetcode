9469407ab1b4b7b981959f11ef8fbe2c47c0fb7e
Hideaki Ishijima <hideaki.ishijima@linecorp.com>
2020-12-17 19:26:12 +0900

    add: add Dec week3 3569 Runtime: 60 ms Memory Usage: 58.3 MB

---
+++ src/main/leetcode/hdkshjm/challenge/month12/week3/chap3569/Solution.java
@@ -0,0 +1,26 @@
+package leetcode.hdkshjm.challenge.month12.week3.chap3569;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    // o(n^2)
+    // Runtime: 60 ms
+    // Memory Usage: 58.3 MB
+    public int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
+        //sum, count
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int a : A) {
+            for (int b : B) {
+                map.put(a + b, map.getOrDefault(a + b, 0) + 1);
+            }
+        }
+        int result = 0;
+        for (int c : C) {
+            for (int d : D) {
+                result += map.getOrDefault(-c - d, 0);
+            }
+        }
+        return result;
+    }
+}
