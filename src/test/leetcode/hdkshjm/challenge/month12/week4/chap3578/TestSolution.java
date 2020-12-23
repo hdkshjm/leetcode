f2b0e193f0fca040f7e7638c3fa8cf95ac5c6cd4
Hideaki Ishijima <hideaki.ishijima@linecorp.com>
2020-12-23 22:45:53 +0900

    add: add Dec week4 3578 Runtime: 0 ms Memory Usage: 35.7 MB

---
+++ src/test/leetcode/hdkshjm/challenge/month12/week4/chap3578/TestSolution.java
@@ -0,0 +1,30 @@
+package leetcode.hdkshjm.challenge.month12.week4.chap3578;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class TestSolution {
+    Solution solution = new Solution();
+
+    static Stream<Arguments> testProvider() {
+        return Stream.of(
+                Arguments.of(12, 21),
+                Arguments.of(21, -1),
+                Arguments.of(132, 213),
+                Arguments.of(1234, 1243),
+                Arguments.of(1999999999, 1999999999)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("testProvider")
+    void test(int n, int expected) {
+        System.out.println("n:  " + n);
+        int actual = solution.nextGreaterElement(n);
+        Assertions.assertEquals(expected, actual);
+    }
+}
