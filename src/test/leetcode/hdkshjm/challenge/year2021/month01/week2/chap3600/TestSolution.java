0bc8b044e3bb59e3ff9a2ca4735d085c7c66fd8d
Hideaki Ishijima <hideaki.ishijima@linecorp.com>
2021-01-12 00:04:18 +0900

    fix: remove unused import 3600

--- src/test/leetcode/hdkshjm/challenge/year2021/month01/week2/chap3600/TestSolution.java
+++ src/test/leetcode/hdkshjm/challenge/year2021/month01/week2/chap3600/TestSolution.java
@@ -5,11 +5,6 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.stream.Stream;
 
 public class TestSolution {
