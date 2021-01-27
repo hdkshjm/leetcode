a998b7015906d20148e1f8d3c0a9deac18a68059
Hideaki Ishijima <hideaki.ishijima@linecorp.com>
2021-01-27 23:16:47 +0900

    fis: fis 3618

--- src/main/leetcode/hdkshjm/challenge/year2021/month01/week4/chap3618/Solution.java
+++ src/main/leetcode/hdkshjm/challenge/year2021/month01/week4/chap3618/Solution.java
@@ -4,10 +4,8 @@ class Solution {
     public int concatenatedBinary(int n) {
         int MODULO = 1000000007;
         long ret = 0; // 計算してる時に桁溢れしちゃうので
-        int shift = 0;
         for (int i = 1; i <= n; i++) {
-            shift =  32 - Integer.numberOfLeadingZeros(i);
-            ret = ((ret << shift) + i);
+            ret = (ret << (32 - Integer.numberOfLeadingZeros(i))) + i;
             if (ret > MODULO) ret %= MODULO;
         }
         return (int) ret;
