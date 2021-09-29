b691bb187061a08d6271c1516375d209e1105f64
Hideaki Ishijima <hideaki.ishijima@linecorp.com>
2021-09-29 19:32:25 +0900

    fix: fix test code....

--- src/test/leetcode/hdkshjm/challenge/year2021/month10/week4/chap3990/TestSolution.java
+++ src/test/leetcode/hdkshjm/challenge/year2021/month10/week4/chap3990/TestSolution.java
@@ -1,10 +1,11 @@
-package leetcode.hdkshjm.challenge.year2021.month10.week4.chap3989;
+package leetcode.hdkshjm.challenge.year2021.month10.week4.chap3990;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.Arrays;
 import java.util.stream.Stream;
 
 public class TestSolution {
@@ -12,16 +13,16 @@ public class TestSolution {
 
     static Stream<Arguments> testProvider() {
         return Stream.of(
-                Arguments.of(new String[]{"test.email+alex@leetcode.com", "test.e.mail+bob.cathy@leetcode.com", "testemail+david@lee.tcode.com"}, 2),
-                Arguments.of(new String[]{"a@leetcode.com", "b@leetcode.com", "c@leetcode.com"}, 3),
-                Arguments.of(new String[]{"test.email+alex@leetcode.com", "test.email.leet+alex@code.com"}, 2)
+                Arguments.of(new int[]{4, 2, 5, 7}, new int[]{4, 5, 2, 7}),
+                Arguments.of(new int[]{2, 3}, new int[]{2, 3})
         );
     }
 
     @ParameterizedTest
     @MethodSource("testProvider")
-    void test(String[] emails, int expected) {
-        int actual = solution.numUniqueEmails(emails);
-        Assertions.assertEquals(expected, actual);
+    void test(int[] nums, int[] expected) {
+        int[] actual = solution.sortArrayByParityII(nums);
+        System.out.println(Arrays.toString(actual));
+        Assertions.assertArrayEquals(expected, actual);
     }
 }
