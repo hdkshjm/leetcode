3bf8adb207faa5ddb6c28fc4eb189aa2456c3e7d
Hideaki Ishijima <hideaki.ishijima@linecorp.com>
2021-10-13 22:40:16 +0900

    add: add 2021-10-13 give up..... 1008. Construct Binary Search Tree from Preorder Traversal

---
+++ src/main/leetcode/hdkshjm/problem/no1000_1999/no1000_1199/no1008/Solution.java
@@ -0,0 +1,22 @@
+package leetcode.hdkshjm.problem.no1000_1999.no1000_1199.no1008;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+class Solution {
+    public TreeNode bstFromPreorder(int[] preorder) {
+        Deque<TreeNode> q = new ArrayDeque<>();
+        TreeNode root = new TreeNode(preorder[0]);
+        q.addFirst(root);
+
+        for (int i = 1; i < preorder.length; i++) {
+            TreeNode node = q.peekFirst();
+            TreeNode child = new TreeNode(preorder[i]);
+            while (!q.isEmpty() && q.peekFirst().val < child.val) node = q.pollFirst();
+            if (node.val < child.val) node.right = child;
+            else node.left = child;
+            q.addFirst(child);
+        }
+        return root;
+    }
+}
