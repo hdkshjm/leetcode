198a84298d71946e66ab852413c37d0304186caa
Hideaki Ishijima <hideaki.ishijima@linecorp.com>
2021-10-25 14:21:25 +0900

    add: add 2021-10-24 222. Count Complete Tree Nodes https://leetcode.com/problems/count-complete-tree-nodes/ Runtime: 0 ms Memory Usage: 41.6 MB

---
+++ src/main/leetcode/hdkshjm/problem/no0000_0999/no0000_0099/no0222/Solution.java
@@ -0,0 +1,25 @@
+package leetcode.hdkshjm.problem.no0000_0999.no0000_0099.no0222;
+
+class Solution {
+    public int countNodes(TreeNode root) {
+        if (root == null) return 0;
+
+        int depth = 1;
+        TreeNode left = root;
+        while (left.left != null) {
+            depth++;
+            left = left.left;
+        }
+
+        return (int) Math.pow(2, depth) - 1 - countNullNodes(root, 1, depth);
+    }
+
+    public int countNullNodes(TreeNode root, int current, int depth) {
+        if (root == null) return 1;
+        if (current == depth) return 0;
+
+        int num = countNullNodes(root.right, current + 1, depth);
+        if (num > 0) num += countNullNodes(root.left, current + 1, depth);
+        return num;
+    }
+}
