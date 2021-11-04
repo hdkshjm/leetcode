d01965e9bf2804e8601a0c2c64cf03ba233ced5c
Hideaki Ishijima <hideaki.ishijima@linecorp.com>
2021-11-04 22:45:16 +0900

    add: add 2021-11-04 404. Sum of Left Leaves https://leetcode.com/problems/sum-of-left-leaves/ Runtime: 0 ms Memory Usage: 37.1 MB

---
+++ src/main/leetcode/hdkshjm/problem/no0000_0999/no0000_0099/no0404/Solution.java
@@ -0,0 +1,11 @@
+package leetcode.hdkshjm.problem.no0000_0999.no0000_0099.no0404;
+
+class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+        if (root == null) return 0;
+        int total = 0;
+        if (root.left != null && root.left.left == null && root.left.right == null) total = root.left.val;
+        total += sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right);
+        return total;
+    }
+}
