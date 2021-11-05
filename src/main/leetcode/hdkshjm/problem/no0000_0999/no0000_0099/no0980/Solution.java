b74ad515221ec62c451a728506ec749988c44fd7
Hideaki Ishijima <hideaki.ishijima@linecorp.com>
2021-11-05 12:27:20 +0900

    fix: fix 2021-11-02 980. Unique Paths III https://leetcode.com/problems/unique-paths-iii/ Runtime: 4 ms

--- src/main/leetcode/hdkshjm/problem/no0000_0999/no0000_0099/no0980/Solution.java
+++ src/main/leetcode/hdkshjm/problem/no0000_0999/no0000_0099/no0980/Solution.java
@@ -18,33 +18,26 @@ class Solution {
             }
         }
 
-        int[][] direction = new int[][]{{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                for (int k = 0; k < direction.length; k++) {
-                    int x = i + direction[k][0];
-                    int y = j + direction[k][1];
-                    if (0 <= x && x < grid.length && 0 <= y && y < grid[0].length && nodes[i][j] != null) {
-                        nodes[i][j].neighbors[k] = nodes[x][y];
-                    }
-                }
-            }
-        }
-
-        int ret = findPath(grid, start, end, new Stack<Node>(), depth);
+        int ret = findPath(nodes, start, end, new Stack<Node>(), depth);
 
         return ret;
     }
 
-    public int findPath(int[][] grid, Node start, Node end, Stack<Node> path, int depth) {
+    public int findPath(Node[][] nodes, Node start, Node end, Stack<Node> path, int depth) {
         if (path.contains(start)) return 0;
         if (path.size() == depth - 1 && start == end) return 1;
+        if (path.size() != depth - 1 && start == end) return 0;
 
         path.add(start);
         int numOfPath = 0;
-        for (Node node : start.neighbors) {
-            if (node == null) continue;
-            numOfPath += findPath(grid, node, end, path, depth);
+
+        int[][] direction = new int[][]{{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
+        for (int k = 0; k < direction.length; k++) {
+            int x = start.x + direction[k][0];
+            int y = start.y + direction[k][1];
+            if (0 <= x && x < nodes.length && 0 <= y && y < nodes[0].length && nodes[x][y] != null) {
+                numOfPath += findPath(nodes, nodes[x][y], end, path, depth);
+            }
         }
         path.pop();
         return numOfPath;
@@ -52,13 +45,11 @@ class Solution {
 }
 
 class Node {
-    Node[] neighbors;
     int x;
     int y;
 
     Node(int x, int y) {
         this.x = x;
         this.y = y;
-        this.neighbors = new Node[4];
     }
 }
