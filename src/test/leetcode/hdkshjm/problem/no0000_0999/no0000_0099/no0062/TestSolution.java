eed5db4aca4a2f05bd397b4eaa86496710779d16
Hideaki Ishijima <hideaki.ishijima@linecorp.com>
2021-11-17 18:11:49 +0900

    add: 2021-11-17 62. Unique Paths https://leetcode.com/problems/unique-paths/ Runtime: 0 ms Memory Usage: 35.8 MB

---
+++ src/test/leetcode/hdkshjm/problem/no0000_0999/no0000_0099/no0062/TestSolution.java
@@ -0,0 +1,25 @@
+package leetcode.hdkshjm.problem.no0000_0999.no0000_0099.no0062;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class TestSolution {
+Solution solution = new  Solution();
+
+    static Stream<Arguments> testProvider() {
+        return Stream.of(
+                Arguments.of(3, 7,28)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("testProvider")
+    void test(int m, int n, int expected) {
+        int actual = solution.uniquePaths(m,n);
+        Assertions.assertEquals(expected, actual);
+    }
+}
