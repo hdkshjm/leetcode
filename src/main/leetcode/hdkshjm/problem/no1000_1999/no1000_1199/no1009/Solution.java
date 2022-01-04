0dcdf66c7c9a6092e24a74149fa93d7fb6ad4c38
Hideaki Ishijima <hideaki.ishijima@linecorp.com>
2022-01-04 21:31:43 +0900

    add: 2021-01-04 https://leetcode.com/problems/complement-of-base-10-integer/submissions/ 0 ms	35.8 MB

---
+++ src/main/leetcode/hdkshjm/problem/no1000_1999/no1000_1199/no1009/Solution.java
@@ -0,0 +1,14 @@
+package leetcode.hdkshjm.problem.no1000_1999.no1000_1199.no1009;
+
+class Solution {
+    public int bitwiseComplement(int n) {
+        if (n == 0) return 1;
+        return ((Integer.highestOneBit(n) << 1) - 1) ^ n;
+    }
+
+    public int bitwiseComplement2(int n) {
+        int ret = 0;
+        for (char c : Integer.toBinaryString(n).toCharArray()) ret = ret * 2 + ('1' - c);
+        return ret;
+    }
+}
