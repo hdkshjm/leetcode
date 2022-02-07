d31a1e63a4a5bc47a242992a56b3f5c2eb37bd22
Hideaki Ishijima <hideaki.ishijima@linecorp.com>
2022-02-07 21:44:59 +0900

    fix...

--- src/test/leetcode/hdkshjm/problem/no0000_0999/no0000_0099/no0023/TestSolution.java
+++ src/test/leetcode/hdkshjm/problem/no0000_0999/no0000_0099/no0023/TestSolution.java
@@ -1,27 +1,34 @@
-package leetcode.hdkshjm.problem.no0000_0999.no0000_0099.no0013;
+package leetcode.hdkshjm.problem.no0000_0999.no0000_0099.no0023;
 
-import java.util.stream.Stream;
-
+import leetcode.hdkshjm.common.ListNode;
+import leetcode.hdkshjm.common.TreeNode;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-public class TestSolution {
+import java.util.stream.Stream;
 
+public class TestSolution {
     Solution solution = new Solution();
 
     static Stream<Arguments> testProvider() {
         return Stream.of(
-                Arguments.of("III", 3),
-                Arguments.of("MCMXCIV", 1994)
+                Arguments.of(new int[][]{{1, 4, 5}, {1, 3, 4}, {2, 6}}, new int[]{1, 1, 2, 3, 4, 4, 5, 6}),
+                Arguments.of(new int[][]{}, new int[]{}),
+                Arguments.of(new int[][]{{}}, new int[]{})
         );
     }
 
     @ParameterizedTest
     @MethodSource("testProvider")
-    void test(String s, int expected) {
-        int result = solution.romanToInt(s);
-        Assertions.assertEquals(expected, result);
+    void test(int[][] listValues, int[] expectedValues) {
+        ListNode expected = new ListNode(expectedValues);
+        ListNode[] lists = new ListNode[listValues.length];
+        for (int i = 0; i < lists.length; i++) lists[i] = ListNode.create(listValues[i]);
+
+        ListNode actual = solution.mergeKLists(lists);
+        if (lists.length == 0 || expectedValues.length == 0) Assertions.assertNull(actual);
+        else ListNode.assertEquals(expected, actual);
     }
 }
