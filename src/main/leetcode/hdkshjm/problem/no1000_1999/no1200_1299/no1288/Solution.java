8081f8d982c1eaab6573c7dcb1589ef30b87faf5
Hideaki Ishijima <hideaki.ishijima@linecorp.com>
2022-02-21 20:41:00 +0900

    add 2022-02-20 9 ms	46.3 MB https://leetcode.com/problems/remove-covered-intervals/submissions/

---
+++ src/main/leetcode/hdkshjm/problem/no1000_1999/no1200_1299/no1288/Solution.java
@@ -0,0 +1,23 @@
+package leetcode.hdkshjm.problem.no1000_1999.no1200_1299.no1288;
+
+import java.util.Arrays;
+
+class Solution {
+    public int removeCoveredIntervals(int[][] intervals) {
+        if (intervals.length == 0) return 0;
+
+        Arrays.sort(intervals, (o1, o2) -> {
+            if (o1[0] == o2[0]) return o2[1] - o1[1];
+            return o1[0] - o2[0];
+        });
+
+        int count = intervals.length;
+        int[] previous = new int[]{intervals[0][0] - 2, intervals[0][0] - 1};
+        for (int[] interval : intervals) {
+            if (previous[0] <= interval[0] && interval[1] <= previous[1]) count--;
+            else previous = interval;
+        }
+
+        return count;
+    }
+}
