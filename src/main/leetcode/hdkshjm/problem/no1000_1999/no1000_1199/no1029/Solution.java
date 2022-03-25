ce5e98b6046e2bfdd40563a04c6d9b60c8d48322
Hideaki Ishijima <hideaki.ishijima@linecorp.com>
2022-03-25 21:48:36 +0900

    add: 2022-03-25 https://leetcode.com/problems/two-city-scheduling/submissions/ 2 ms	41.5 MB

---
+++ src/main/leetcode/hdkshjm/problem/no1000_1999/no1000_1199/no1029/Solution.java
@@ -0,0 +1,13 @@
+package leetcode.hdkshjm.problem.no1000_1999.no1000_1199.no1029;
+
+import java.util.Arrays;
+
+class Solution {
+    public int twoCitySchedCost(int[][] costs) {
+        Arrays.sort(costs, (o1, o2) -> o1[0] - o2[0] - (o1[1] - o2[1]));
+        int total = 0;
+        int half = costs.length / 2;
+        for (int i = 0; i < costs.length / 2; i++) total += costs[i][0] + costs[i + half][1];
+        return total;
+    }
+}
---
+++ src/test/leetcode/hdkshjm/problem/no1000_1999/no1000_1199/no1029/TestSolution.java
@@ -0,0 +1,28 @@
+package leetcode.hdkshjm.problem.no1000_1999.no1000_1199.no1029;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class TestSolution {
+    Solution solution = new Solution();
+
+    static Stream<Arguments> testProvider() {
+        return Stream.of(
+                Arguments.of(new int[][]{{10, 20}, {30, 200}, {400, 50}, {30, 20}}, 110),
+                Arguments.of(new int[][]{{20, 10}, {200, 30}, {50, 400}, {20, 30}}, 110),
+                Arguments.of(new int[][]{{259, 770}, {448, 54}, {926, 667}, {184, 139}, {840, 118}, {577, 469}}, 1859),
+                Arguments.of(new int[][]{{515, 563}, {451, 713}, {537, 709}, {343, 819}, {855, 779}, {457, 60}, {650, 359}, {631, 42}}, 3086)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("testProvider")
+    void test(int[][] costs, int expected) {
+        int actual = solution.twoCitySchedCost(costs);
+        Assertions.assertEquals(expected, actual);
+    }
+}
